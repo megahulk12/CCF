f9b43517212c0d918b71ab3d43bdc7f62c31349b
megahulk12 <paoloacosta50@icloud.com>
2017-07-03 18:06:06 +0800

    weee

--- register.php
+++ register.php
@@ -601,7 +601,6 @@
 			$('.datepicker').pickadate({
 				selectMonths: true, // Creates a dropdown to control month
 				selectYears: 50, // Creates a dropdown of 15 years to control year
-				formatSubmit: 'yyyy-mm-dd',
 				max: true
 			});
 			 
