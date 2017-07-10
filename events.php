0b7eeae5a0b52ed04d7dd9868f7dc0727f1efe3c
butchersix <jasperhavenbriones@gmail.com>
2017-07-10 10:57:09 +0800

    another update

--- events.php
+++ events.php
@@ -1,4 +1,7 @@
-<?php include('session.php'); ?>
+<?php 
+	include('session.php'); 
+	include('globalfunctions.php'); 
+?>
 <?xml version = ″1.0″?>
 <!DOCTYPE html PUBLIC ″-//w3c//DTD XHTML 1.1//EN″ “http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd”>
 <html xmlns = ″http://www.w3.org/1999/xhtml″>
