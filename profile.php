db9e07c0332a1287a0d4b6640feb42c1424e1e86
smsamodal <samumqsamodal@addu.edu.ph>
2017-08-07 19:53:25 +0800

    fix unnecessary pull lines

--- profile.php
+++ profile.php
@@ -2091,10 +2091,9 @@
 <<<<<<< HEAD
 		/*
 		$("#coinfo_next").click(function() {
-=======
+
 		$("#cprefer_next").click(function() {
 			// default states
->>>>>>> 85627922acf9d52d06f038765eb58eeb99f17f90
 			$('.error').hide();
 			$(this).blur(); // no focus in button once clicked
 			var check_iteration = true;
@@ -2169,10 +2168,10 @@
 			}
 		});
 
-<<<<<<< HEAD
+
 			});
 			*/
-=======
+
 		function checkLastPage() {
 			var currentpageid = getCurrentPage(), pagelength = currentpageid.length, pagenumber = currentpageid.charAt(pagelength-1);
 			pagenumber++; // page that is after the previous
@@ -2197,7 +2196,7 @@
 			var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
 			return pattern.test(emailAddress);
 		};
->>>>>>> 85627922acf9d52d06f038765eb58eeb99f17f90
+
 
 		// change password form validation
 		$("#submit_cpass").click(function() {
