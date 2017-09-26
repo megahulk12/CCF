8bba0a036b36e5907e9280f95bd920cb00259f45
megahulk12 <paoloacosta50@icloud.com>
2017-09-26 09:37:18 +0800

    [fixed]
    [fixed]
    1. nawala ang changes ko sa freelancer at unemployed so nandito na sya ulit

--- profile.php
+++ profile.php
@@ -1940,6 +1940,12 @@
 				company.hide();
 				$(".company input").prop("required", false);
 			}
+			else if($(professionid).val().toLowerCase() == "unemployed" || $(professionid).val().toLowerCase() == "freelancer"){
+				company.hide();
+				$(".company input").prop("required", false);
+				school.hide();
+				$(".school input").prop("required", false);
+			}
 			else {
 				school.hide();
 				$(".school input").prop("required", false);
